f96c18f7e570e2c393712c01b4d9bae9b7cc7675
Nathan Klingensmith <paradoxpyt@gmail.com>
2023-04-11 20:37:36 -0400

    Add MUI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,37 @@
-import { Roboto } from 'next/font/google';
+import { ThemeProvider } from '@emotion/react';
+import { Inter } from 'next/font/google';
+
+import { ShoppingCartOutlined } from '@mui/icons-material';
+import { createTheme } from '@mui/material';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+
 import styles from './index.module.scss';
 
-const roboto = Roboto({
+const inter = Inter({
   weight: ['300', '400', '500', '700'],
-  style: ['normal', 'italic'],
+  style: ['normal'],
   subsets: ['latin']
 });
 
+const theme = createTheme({
+  palette: {
+    mode: 'dark'
+  },
+  typography: {
+    fontFamily: inter.style.fontFamily
+  }
+});
+
 export default function Home() {
-  return <main className={`${roboto.className} ${styles.main}`}>Hello world!</main>;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <main className={`${inter.className} ${styles.main}`}>
+        <Button variant="contained">
+          <ShoppingCartOutlined fontSize="small" /> Checkout
+        </Button>
+      </main>
+    </ThemeProvider>
+  );
 }
