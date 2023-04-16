2f7d826d51aedabe642a8031faed638f93ab32b2
Nathan Klingensmith <paradoxpyt@gmail.com>
2023-04-16 01:50:32 -0400

    Use next/image

--- components/Splash/Splash.tsx
+++ components/Splash/Splash.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 import styles from './Splash.module.scss';
 
@@ -5,7 +6,7 @@ export const Splash: React.FC = () => {
   return (
     <div className={styles.splash}>
       <div className={styles.splash__title}>Find just what you were looking for</div>
-      <img src="/splash.png" alt="Splash image" />
+      <Image src="/splash.png" alt="Splash image" width={1200} height={700} priority />
     </div>
   );
 };
